00db12d89445c8ef5fdce12d1ea1fe7bfb0d266f
Anton Agafonov <tkmtldn@gmail.com>
2020-12-15 01:46:13 +0400

    day 14, changes

--- day_14/day14.go
+++ day_14/day14.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -11,6 +12,9 @@ import (
 )
 
 // https://adventofcode.com/2020/day/14
+
+var mask string
+var dict2 = map[int]int{}
 
 func ReadData(path string) (elems []string) {
 	file, err := os.Open(path)
@@ -28,24 +32,39 @@ func ReadData(path string) (elems []string) {
 }
 
 func BinString(e string) (key int, new string) {
-	e = strings.Replace(e, "mem[", "",1)
-	e = strings.Replace(e, "]", "",1)
-	e = strings.Replace(e, " ", "",-1)
+	e = strings.Replace(e, "mem[", "", 1)
+	e = strings.Replace(e, "]", "", 1)
+	e = strings.Replace(e, " ", "", -1)
 	f := strings.Split(e, "=")
 	key, _ = strconv.Atoi(f[0])
 	n, _ := strconv.Atoi(f[1])
 	st := strconv.FormatInt(int64(n), 2)
-	for i:=0; i<36-len(st); i++ {
+	for i := 0; i < 36-len(st); i++ {
 		new += "0"
 	}
 	new += st
 	return key, new
 }
 
+func BinString2(e string) (value int, new string) {
+	e = strings.Replace(e, "mem[", "", 1)
+	e = strings.Replace(e, "]", "", 1)
+	e = strings.Replace(e, " ", "", -1)
+	f := strings.Split(e, "=")
+	value, _ = strconv.Atoi(strings.Trim(f[1], ""))
+	n, _ := strconv.Atoi(f[0])
+	st := strconv.FormatInt(int64(n), 2)
+	for i := 0; i < 36-len(st); i++ {
+		new += "0"
+	}
+	new += st
+	return value, new
+}
+
 func CmpMask(m string, e string) (r string) {
 	m1 := strings.Split(m, "")
 	e1 := strings.Split(e, "")
-	for i:=0; i<36; i++ {
+	for i := 0; i < 36; i++ {
 		if m1[i] == "X" {
 			r += e1[i]
 		} else {
@@ -55,24 +74,84 @@ func CmpMask(m string, e string) (r string) {
 	return r
 }
 
-var mask string
+func CmpMask2(m string, e string) (r string) {
+	m1 := strings.Split(m, "")
+	e1 := strings.Split(e, "")
+	for i := 0; i < 36; i++ {
+		if m1[i] == "0" {
+			r += e1[i]
+		} else {
+			r += m1[i]
+		}
+	}
+	return r
+}
 
-func DockingData(inp []string) (sum int){
+func DockingData(inp []string) (sum int) {
 	dict := map[int]int{}
 	for _, e := range inp {
-		if strings.HasPrefix(e, "mask"){
-			f := strings.Replace(e, "mask = ", "",1)
+		if strings.HasPrefix(e, "mask") {
+			f := strings.Replace(e, "mask = ", "", 1)
 			mask = f
 		}
-		if strings.HasPrefix(e, "mem"){
+		if strings.HasPrefix(e, "mem") {
 			key, s := BinString(e)
 			res := CmpMask(mask, s)
 			output, _ := strconv.ParseInt(res, 2, 64)
-			dict[key]=int(output)
+			dict[key] = int(output)
 		}
 	}
-	for _, v:= range dict{
-		sum +=v
+	for _, v := range dict {
+		sum += v
+	}
+	return sum
+}
+
+func ExplorePerm(r string, val int) {
+	count := float64(strings.Count(r, "X"))
+	vars := math.Pow(2, count)
+
+	for i := 0; i < int(vars); i++ {
+		ans := ""
+		integ := strconv.FormatInt(int64(i), 2)
+		for i := 0; i < (int(count) - len(integ)); i++ {
+			ans += "0"
+		}
+		ans += integ
+
+		res := ""
+		pos := 0
+
+		ans1 := strings.Split(ans, "")
+		r1 := strings.Split(r, "")
+		for i := 0; i < 36; i++ {
+			if r1[i] == "X" {
+				res += ans1[pos]
+				pos++
+			} else {
+				res += r1[i]
+			}
+		}
+		output, _ := strconv.ParseInt(res, 2, 64)
+		dict2[int(output)] = val
+	}
+}
+
+func DockingData2(inp []string) (sum int) {
+	for _, e := range inp {
+		if strings.HasPrefix(e, "mask") {
+			f := strings.Replace(e, "mask = ", "", 1)
+			mask = f
+		}
+		if strings.HasPrefix(e, "mem") {
+			val, s := BinString2(e)
+			res := CmpMask2(mask, s)
+
+			ExplorePerm(res, val)
+		}
+	}
+	for _, v := range dict2 {
+		sum += v
 	}
 	return sum
 }
@@ -82,4 +161,5 @@ func main() {
 	inp := ReadData(path)
 
 	fmt.Println("First answer: ", DockingData(inp))
+	fmt.Println("Second answer: ", DockingData2(inp))
 }
--- day_14/day14_test.go
+++ day_14/day14_test.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+func TestDay14(t *testing.T){
+	path := filepath.Join(".", "test.txt")
+	inp := ReadData(path)
+	path2 := filepath.Join(".", "test2.txt")
+	inp2 := ReadData(path2)
+
+	res1 := DockingData(inp)
+	exp1 := 165
+	if res1 != exp1 {
+		t.Errorf("unexpected result on Day 14 Part 1: got %v want %v", res1, exp1)
+	}
+
+	res2 := DockingData2(inp2)
+	exp2 := 208
+	if res2 != exp2 {
+		t.Errorf("unexpected result on Day 14 Part 2: got %v want %v", res2, exp2)
+	}
+}
