69fbbcce29f9d672674645731ab520bb42f48bdf
Anton Agafonov <tkmtldn@gmail.com>
2020-12-19 15:28:15 +0400

    day 18 added

--- day_18/day18.go
+++ day_18/day18.go
@@ -6,49 +6,135 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-// https://adventofcode.com/2020/day/17
+// https://adventofcode.com/2020/day/18
 
-type Cube struct {
-	x     int
-	y     int
-	z     int
-	value string
-}
-
-func ReadData(path string) (elems []Cube) {
+func ReadData(path string) []string {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatalf("Error. Problem with opening file.")
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	cubs := []Cube{}
-	this_y := 0
+	elems := []string{}
 
 	for scanner.Scan() {
 		e := scanner.Text()
-		sl := strings.Split(e,"")
-		for k, v:= range sl{
-			c :=Cube{
-				x: k,
-				y: this_y,
-				z: 0,
-				value: v,
-			}
-			cubs = append(cubs, c)
-		}
-		this_y ++
+		elems = append(elems, e)
 	}
-	return cubs
+	return elems
+}
+
+func CalculateWithoutPriority(s string) string {
+	data := strings.Split(s, " ")
+	result, _ := strconv.Atoi(data[0])
+	for i := 2; i < len(data); i += 2 {
+		operator := data[i-1]
+		y, _ := strconv.Atoi(data[i])
+		switch operator {
+		case "+":
+			result += y
+		case "*":
+			result *= y
+		}
+	}
+	res := strconv.Itoa(result)
+	return res
+}
+
+func CalculateWithPriority(s string) string {
+	data := strings.Split(s, " ")
+	datastr := make([]string, 0)
+
+	for i:=0; i<len(data); i++ {
+		if data[i]=="+"{
+			x, _ := strconv.Atoi(datastr[len(datastr)-1])
+			y, _ := strconv.Atoi(data[i+1])
+			res := x+y
+			z := strconv.Itoa(res)
+			datastr = datastr[:len(datastr)-1]
+			datastr = append(datastr, z)
+			i++
+		} else {
+			datastr = append(datastr, data[i])
+		}
+	}
+
+	result := 1
+	datastrmul := strings.Join(datastr, "")
+	datastrmu := strings.Split(datastrmul, "*")
+
+	for _, e := range datastrmu{
+		int_e, _ := strconv.Atoi(e)
+		result *= int_e
+	}
+	res := strconv.Itoa(result)
+
+	return res
+}
+
+func WithoutPriority(s string) string {
+
+	levels := strings.Count(s, "(")
+	if levels == 0 {
+		return CalculateWithoutPriority(s)
+	} else {
+		start := 0
+		finish := 0
+		for i, v := range s{
+			if v == 40{
+			start = i
+			}
+			if v == 41{
+				finish = i
+				break
+			}
+		}
+		result := CalculateWithoutPriority(s[start+1 : finish])
+		r := s[:start] + result + s[finish+1:]
+		return WithoutPriority(r)
+	}
+	return ""
+}
+
+func WithPriority(s string) string {
+
+	levels := strings.Count(s, "(")
+	if levels == 0 {
+		return CalculateWithPriority(s)
+	} else {
+		start := 0
+		finish := 0
+		for i, v := range s{
+			if v == 40{
+				start = i
+			}
+			if v == 41{
+				finish = i
+				break
+			}
+		}
+		result := CalculateWithPriority(s[start+1 : finish])
+		r := s[:start] + result + s[finish+1:]
+		return WithPriority(r)
+	}
+	return ""
+}
+
+func OperationOrder(inp []string, f func(string)string) (sum int) {
+	for _, e := range inp {
+		res, _ := strconv.Atoi(f(e))
+		sum += res
+	}
+	return sum
 }
 
 func main() {
-	path := filepath.Join(".", "day_17", "test.txt")
+	path := filepath.Join(".", "day_18", "input.txt")
 	inp := ReadData(path)
-	for _, v:= range inp{
-		fmt.Println(v)
-	}
+	fmt.Println("First answer:", OperationOrder(inp, WithoutPriority))
+	fmt.Println("Second answer:", OperationOrder(inp, WithPriority))
 }
--- day_18/day18_test.go
+++ day_18/day18_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestDay18(t *testing.T) {
+	tests1 := []struct {
+		input []string
+		want  int
+	}{
+		{[]string{"1 + 2 * 3 + 4 * 5 + 6"}, 71},
+		{[]string{"1 + (2 * 3) + (4 * (5 + 6))"}, 51},
+		{[]string{"2 * 3 + (4 * 5)"}, 26},
+		{[]string{"5 + (8 * 3 + 9 + 3 * 4 * 3)"}, 437},
+		{[]string{"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"}, 12240},
+		{[]string{"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"}, 13632},
+	}
+	for _, tt := range tests1 {
+		t.Run("WithoutPriority", func(t *testing.T) {
+			got := OperationOrder(tt.input, WithoutPriority)
+			if got != tt.want {
+				t.Errorf("Error when running with value %v, got %v, want %v.", tt.input, got, tt.want)
+			}
+		})
+	}
+
+	tests2 := []struct {
+		input []string
+		want  int
+	}{
+		{[]string{"1 + 2 * 3 + 4 * 5 + 6"}, 231},
+		{[]string{"1 + (2 * 3) + (4 * (5 + 6))"}, 51},
+		{[]string{"2 * 3 + (4 * 5)"}, 46},
+		{[]string{"5 + (8 * 3 + 9 + 3 * 4 * 3)"}, 1445},
+		{[]string{"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"}, 669060},
+		{[]string{"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"}, 23340},
+	}
+	for _, tt := range tests2 {
+		t.Run("WithPriority", func(t *testing.T) {
+			got := OperationOrder(tt.input, WithPriority)
+			if got != tt.want {
+				t.Errorf("Error when running with value %v, got %v, want %v.", tt.input, got, tt.want)
+			}
+		})
+	}
+}
