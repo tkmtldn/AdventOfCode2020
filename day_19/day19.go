d7ace9a32c48edfe5ec95f678a1e88ba85c53e34
Anton Agafonov <tkmtldn@gmail.com>
2020-12-20 14:50:55 +0400

    day 20, changes

--- day_19/day19.go
+++ day_19/day19.go
@@ -6,49 +6,104 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-// https://adventofcode.com/2020/day/17
+// https://adventofcode.com/2020/day/19
 
-type Cube struct {
-	x     int
-	y     int
-	z     int
-	value string
-}
-
-func ReadData(path string) (elems []Cube) {
+func ReadData(path string) (map[string]string, []string) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatalf("Error. Problem with opening file.")
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	cubs := []Cube{}
-	this_y := 0
+
+	data := map[string]string{}
+	messages := []string{}
+	level := 0
 
 	for scanner.Scan() {
 		e := scanner.Text()
-		sl := strings.Split(e,"")
-		for k, v:= range sl{
-			c :=Cube{
-				x: k,
-				y: this_y,
-				z: 0,
-				value: v,
-			}
-			cubs = append(cubs, c)
+		if e == "" {
+			level++
+			continue
 		}
-		this_y ++
+		if level == 0 {
+			e := strings.Replace(e, "\"", "", -1)
+			es := strings.Split(e, ": ")
+			key := es[0]
+			value := es[1]
+			data[key] = value
+		}
+		if level == 1 {
+			messages = append(messages, e)
+		}
 	}
-	return cubs
+	return data, messages
+}
+
+var replacing = map[string]string{}
+
+func CheckIfNumbers(s string) bool {
+	ans := true
+	list := strings.Split(s, " ")
+	for _, e := range list {
+		_, err := strconv.Atoi(e)
+		if err == nil {
+			ans = false
+		}
+	}
+	return ans
+}
+
+func MonsterMessages(d map[string]string, m []string) int {
+
+	up := len(d)
+
+	for k, v := range d {
+		if len(v) == 1 && (v == "a" || v == "b") {
+			replacing[k] = v
+			delete(d, k)
+		}
+	}
+	//fmt.Println(d)
+
+	for {
+		if len(replacing) >= up {
+			break
+		}
+		for k, v := range d {
+			list := strings.Split(v, " ")
+			list2 := []string{}
+			for _, e := range list {
+				if value, ok := replacing[e]; ok {
+					list2 = append(list2, value)
+				} else {
+					list2 = append(list2, e)
+				}
+			}
+			v = strings.Join(list2, " ")
+			//fmt.Println("v=========", v)
+			if CheckIfNumbers(v) {
+				v = strings.Replace(v, " ", "", -1)
+				v = strings.Replace(v, "|", " | ", -1)
+				v = strings.Replace(v, "[", " [ ", -1)
+				v = strings.Replace(v, "]", " ] ", -1)
+				replacing[k] = "[" + v + "]"
+				delete(d, k)
+			}
+		}
+	}
+	for k, v := range replacing {
+		fmt.Println(k, "==", v)
+	}
+	return 0
 }
 
 func main() {
-	path := filepath.Join(".", "day_17", "test.txt")
-	inp := ReadData(path)
-	for _, v:= range inp{
-		fmt.Println(v)
-	}
+	path := filepath.Join(".", "day_19", "test.txt")
+	d, m := ReadData(path)
+	fmt.Println(MonsterMessages(d, m))
 }
--- day_20/day20.go
+++ day_20/day20.go
@@ -6,49 +6,109 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-// https://adventofcode.com/2020/day/17
+// https://adventofcode.com/2020/day/20
 
-type Cube struct {
-	x     int
-	y     int
-	z     int
-	value string
-}
-
-func ReadData(path string) (elems []Cube) {
+func ReadData(path string) (map[int][]string) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatalf("Error. Problem with opening file.")
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	cubs := []Cube{}
-	this_y := 0
+
+	elems := map[int][]string{}
+	k := 0
+	v := []string{}
 
 	for scanner.Scan() {
 		e := scanner.Text()
-		sl := strings.Split(e,"")
-		for k, v:= range sl{
-			c :=Cube{
-				x: k,
-				y: this_y,
-				z: 0,
-				value: v,
-			}
-			cubs = append(cubs, c)
+		if e == "" {
+			elems[k] = Ribs(v)
+			v = []string{}
+			continue
 		}
-		this_y ++
+		if strings.HasPrefix(e, "Tile ") {
+			e = strings.Replace(e, ":", "", -1)
+			el := strings.Split(e, " ")
+			k, _ = strconv.Atoi(el[1])
+			continue
+		}
+		v = append(v, e)
 	}
-	return cubs
+	return elems
+}
+
+func Reverse(s string) (result string) {
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return
+}
+
+func Ribs(s []string) (result []string) {
+	top := ""
+	left := ""
+	right := ""
+	bottom := ""
+
+	limit := len(s) - 1
+	for i, e := range s {
+		dict := strings.Split(e, "")
+		if i == 0 {
+			top += strings.Join(dict, "")
+		}
+		if i == limit {
+			bottom += strings.Join(dict, "")
+		}
+		left += dict[0]
+		right += dict[limit]
+	}
+	result = append(result, top)
+	result = append(result, right)
+	result = append(result, bottom)
+	result = append(result, left)
+	result = append(result, Reverse(top))
+	result = append(result, Reverse(right))
+	result = append(result, Reverse(bottom))
+	result = append(result, Reverse(left))
+	return
+}
+
+func JurassicJigsaw(inp map[int][]string) int {
+	data := []string{}
+	for _, v := range inp {
+		data = append(data, v...)
+	}
+
+	values := map[int]int{}
+	for k, v := range inp {
+		count := 0
+		for _, vv := range v {
+			for _, e := range data {
+				if vv == e {
+					count++
+				}
+			}
+		}
+		values[k] = (count - 8) / 2
+	}
+
+	result := 1
+	for k, v := range values {
+		fmt.Println(k, v)
+		if v == 2 {
+			result *= k
+		}
+	}
+	return result
 }
 
 func main() {
-	path := filepath.Join(".", "day_17", "test.txt")
+	path := filepath.Join(".", "day_20", "test.txt")
 	inp := ReadData(path)
-	for _, v:= range inp{
-		fmt.Println(v)
-	}
+	fmt.Println("First answer:", JurassicJigsaw(inp))
 }
