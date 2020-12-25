68b3e92e33b049ccf7b3f047e4c717416f90fca8
Anton Agafonov <tkmtldn@gmail.com>
2020-12-26 00:01:38 +0400

    day 25, changes

--- day_25/day25.go
+++ day_25/day25.go
@@ -5,36 +5,31 @@ import (
 )
 
 func Transform(pk int) int {
-	l := 0
-	v := 1
-	s := 7
+	loop := 0
+	value := 1
+	subj_number := 7
 	for {
-		if v == pk {
-			return l
+		if value == pk {
+			return loop
 		}
-		l++
-		v *= s
-		v %= 20201227
+		loop++
+		value *= subj_number
+		value %= 20201227
 	}
 }
 
 func ComboBreaker(card int, door int) int {
-	c := Transform(card)
 	d := Transform(door)
-	fmt.Println(c, d)
-	v := 1
+	value := 1
 	for i := 1; i <= d; i++ {
-		v *= card
-		v %= 20201227
+		value *= card
+		value %= 20201227
 	}
-	return v
+	return value
 }
 
 func main() {
-	cardPKtest := 5764801
-	doorPKtest := 17807724
 	cardPK := 8458505
 	doorPK := 16050997
-	fmt.Println(ComboBreaker(cardPKtest, doorPKtest))
 	fmt.Println(ComboBreaker(cardPK, doorPK))
 }
